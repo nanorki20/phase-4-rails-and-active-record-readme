1a314a50240f40a4de28691f2e86f4f50e4d19da
Hayet Kumasseh <naaakleh@gmail.com>
2022-07-19 15:40:13 -0400

    commit tree f2986182ba72ebb13ac0f7dd56039f9d011f9a93

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Clearing out old data..'
+Cheese.destroy_all
+
+puts 'generating new data..'
+Cheese.create!(name: 'Cheddar', price: 3, is_best_seller: true)
+Cheese.create!(name: 'Pepper Jack', price: 4, is_best_seller: true)
+Cheese.create!(name: 'Limburger', price: 8, is_best_seller: false)
+Cheese.create!(name: 'Gouda', price: 7, is_best_seller: true)
+Cheese.create!(name: 'American', price: 2, is_best_seller: false)
+puts 'Done!'
